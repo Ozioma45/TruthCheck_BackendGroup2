53f8ea24697f305a13f01be77052f1ce202dfc2d
Ozioma45 <oziomaegole@gmail.com>
2025-04-09 13:11:48 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const hostname = "localhost";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, welcome to my Node.js server!");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
